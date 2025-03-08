ba916ef562d8f478c666bb1d5992502c4b47eae0
nofcngway <nofcngway@gmail.com>
2025-03-08 22:20:47 +0500

    fix

--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,7 @@
-import yaml from "js-yaml";
+import yaml from 'js-yaml';
 
 const parseData = (data, format) => {
-  const parsers = { json: JSON.parse, yaml: yaml.load, yml: yaml.load, };
+  const parsers = { json: JSON.parse, yaml: yaml.load, yml: yaml.load };
   return parsers[format](data);
 };
 

aeab8f14f9db6e4f2bbe50c20f9578a2746b2eca
nofcngway <nofcngway@gmail.com>
2025-03-14 15:05:36 +0500

    fix

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,15 +1,3 @@
-const processingData = (diff) => {
-  const result = {};
-  diff.forEach((item) => {
-    result[item.key] = item.data;
-  });
-
-  return result;
-};
-
-const formatJson = (diff) => {
-  const obj = processingData(diff);
-  return JSON.stringify(obj, null, 2);
-};
+const formatJson = (diff) => JSON.stringify(diff, null, 2);
 
 export default formatJson;

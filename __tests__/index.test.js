157512e34d10d19916001327ad8b7988eb03db7a
nofcngway <nofcngway@gmail.com>
2025-03-14 17:24:56 +0500

    upd tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,6 +2,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
 import compareFiles from '../src/index.js';
+import formatJson from '../src/formatters/json.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,16 +13,18 @@ test('stylish - json', () => {
   const firstFilename = getFixturePath('file1.json');
   const secondFilename = getFixturePath('file2.json');
   const resultName = getFixturePath('result_stylish.txt');
-  const result = fs.readFileSync(resultName, 'utf8');
-  expect(compareFiles(firstFilename, secondFilename)).toEqual(result);
+  const expectedValue = fs.readFileSync(resultName, 'utf8');
+  const actualValue = compareFiles(firstFilename, secondFilename);
+  expect(actualValue).toEqual(expectedValue);
 });
 
 test('stylish - yml', () => {
   const firstFilename = getFixturePath('file1.yml');
   const secondFilename = getFixturePath('file2.yml');
   const resultName = getFixturePath('result_stylish.txt');
-  const result = fs.readFileSync(resultName, 'utf8');
-  expect(compareFiles(firstFilename, secondFilename)).toEqual(result);
+  const expectedValue = fs.readFileSync(resultName, 'utf8');
+  const actualValue = compareFiles(firstFilename, secondFilename);
+  expect(actualValue).toEqual(expectedValue);
 });
 
 test('plain - json', () => {
@@ -29,8 +32,9 @@ test('plain - json', () => {
   const secondFilename = getFixturePath('file2.json');
   const formatName = 'plain';
   const resultName = getFixturePath('result_plain.txt');
-  const result = fs.readFileSync(resultName, 'utf8');
-  expect(compareFiles(firstFilename, secondFilename, formatName)).toEqual(result);
+  const expectedValue = fs.readFileSync(resultName, 'utf8');
+  const actualValue = compareFiles(firstFilename, secondFilename, formatName);
+  expect(actualValue).toEqual(expectedValue);
 });
 
 test('plain - yml', () => {
@@ -38,6 +42,45 @@ test('plain - yml', () => {
   const secondFilename = getFixturePath('file2.yml');
   const formatName = 'plain';
   const resultName = getFixturePath('result_plain.txt');
-  const result = fs.readFileSync(resultName, 'utf8');
-  expect(compareFiles(firstFilename, secondFilename, formatName)).toEqual(result);
+  const expectedValue = fs.readFileSync(resultName, 'utf8');
+  const actualValue = compareFiles(firstFilename, secondFilename, formatName);
+  expect(actualValue).toEqual(expectedValue);
+});
+
+test('json - json', () => {
+  const diff = [
+    { key: 'host', data: 'hexlet.io', char: 'general' },
+    { key: 'timeout', data: { first: 50, second: 20 }, char: 'different' },
+    { key: 'proxy', data: '123.234.53.22', char: 'add' },
+    { key: 'verbose', data: true, char: 'remove' },
+  ];
+
+  const actualValue = formatJson(diff);
+  const expectedValue = `[
+  {
+    "key": "host",
+    "data": "hexlet.io",
+    "char": "general"
+  },
+  {
+    "key": "timeout",
+    "data": {
+      "first": 50,
+      "second": 20
+    },
+    "char": "different"
+  },
+  {
+    "key": "proxy",
+    "data": "123.234.53.22",
+    "char": "add"
+  },
+  {
+    "key": "verbose",
+    "data": true,
+    "char": "remove"
+  }
+]`;
+
+  expect(actualValue).toEqual(expectedValue);
 });
